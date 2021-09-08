7badbd337c400d9d8f780a7761ed56c94f68b7a6
Isabellanamutenda <namtendaisabella@.com>
2021-09-08 20:32:50 +0300

    Make changes o the index file

---
+++ src/error.js
@@ -0,0 +1,14 @@
+export default (message, color) => {
+  const parent = document.getElementById('container');
+  const error = document.createElement('aside');
+  error.textContent = message;
+  error.id = 'error-popup';
+  error.classList.add(color);
+  setTimeout(() => {
+    error.classList.add('fade');
+  }, 500);
+  parent.appendChild(error);
+  setTimeout(() => {
+    parent.removeChild(document.getElementById('error-popup'));
+  }, 3500);
+};
